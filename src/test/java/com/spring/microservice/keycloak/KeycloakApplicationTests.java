0d3681aa96f67a2b1cc930587bf6cd277d73654f
Paris Liu <paris0120@gmail.com>
2022-07-02 10:15:53 -0400

    init

---
+++ src/test/java/com/spring/microservice/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservice.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
